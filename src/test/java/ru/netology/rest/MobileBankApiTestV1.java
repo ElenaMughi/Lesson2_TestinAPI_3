e27865c4e83dcaeeb8d53b44f7cfec3c0f3f71ae
ElenaMughi <elena.mughi@gmail.com>
2022-03-26 23:16:27 +0300

    try2

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,27 +3,27 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-        // Given - When - Then
-        // Предусловия
         given()
-//                .baseUri("http://localhost:9999/api/v1")
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("{\"id\":4,\"name\":\"Текущий счет\",\"number\":\"1111111112\",\"balance\":0,\"currency\":\"USD\"}")
+//                .body("add{\"id\"~4;\"name\"~\"Текущий зарплатный счёт\";\"number\"~1111111112;\"balance\"~110;\"currency\"~\"USD\"}")
+//                .body("{\"id\"=4,\"name\"=\"Текущий зарплатный счёт\",\"number\"=1111111112,\"balance\"=110,\"currency\"=\"USD\"}")
+                .body("Текущий зарплатный счёт")
                 // Выполняемые действия
                 .when()
-//                .post("/demo/accounts")
                 .post("/post")
+
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("name", equalTo("Текущий счет"));
+//                .body("data.name", equalTo("Текущий зарплатный счёт"));
+                .body("data", equalTo("Текущий зарплатный счёт"));
 
     }
 }
+
